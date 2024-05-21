3cdfb5fe5f4424395738c31055ebdb1824fa50b9
unknown <archanaraavi2608@gmail.com>
2024-05-21 16:30:07 +0100

    components created

--- src/App.js
+++ src/App.js
@@ -1,25 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+import Table from "./components/table/table.js"
+import Image from "./components/image/image.js"
+import Form from "./components/form/form.js"
+import Button from "./components/button/button.js"
+import List from "./components/list/list.js"
+function Hello()
+{
+  return(
+    <div>
+      <h1>hello world</h1>
+      <Table/>
+      <Image/>
+      <Form/><br/>
+      <Button/>
+      <List/>
     </div>
-  );
+  )
 }
-
-export default App;
+export default Hello
---
+++ src/components/form/form.js
@@ -0,0 +1,11 @@
+const Form=()=>{
+    return(
+        <forms><br/>
+        <input type="text" placeholder="username"></input><br/>
+        <input type="text" placeholder="password"></input>
+        </forms>
+    )
+}
+export default Form  
+
+    
